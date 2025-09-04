ad7c36b525a90aa1ba8c19f189ea202665bf2f39
Richard Lanchez <richardsunny2015@gmail.com>
2025-09-04 13:30:08 -0400

    Exclude previous winning numbers

--- src/powerball_shuffle/core.clj
+++ src/powerball_shuffle/core.clj
@@ -1,5 +1,41 @@
 (ns powerball-shuffle.core
-  (:require [clojure.string :as str]))
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [clojure.string :as str]))
+
+(defn- csv-data->maps 
+  [csv-data]
+  (map zipmap
+       (->> (first csv-data) ;; First row is the header
+            (repeat))
+       (rest csv-data)))
+
+(defn- massage-record
+  [record]
+  (let [winning-numbers (-> record 
+                            (get "Winning Numbers")
+                            (str/split #" "))]
+    (mapv #(Integer/parseInt %) winning-numbers)))
+
+(defn- massage-records
+  "Takes sets of records and returns the winning
+   numbers as a set."
+  [records]
+  (->> records 
+       (map massage-record)
+       (set)))
+
+(defn- read-previous-powerball-numbers
+  "Reads csv file from PAST_POWERBALL env variable
+   and creates a set of previous powerball numbers"
+  []
+  (with-open [r (io/reader (System/getenv "PAST_POWERBALL"))]
+    (let [results (-> r
+                      (csv/read-csv)
+                      (csv-data->maps))]
+      (massage-records results))))
+
+(def previous-powerball-numbers (read-previous-powerball-numbers))
 
 (defn- powerball-numbers
   []
@@ -10,8 +46,11 @@
                           (vec))
         powerball (->> (range 1 27)
                        (shuffle)
-                       (first))]
-    (conj main-numbers powerball)))
+                       (first))
+        numbers (conj main-numbers powerball)]
+    (if-not (contains? previous-powerball-numbers numbers)
+      numbers
+      (println "Generated existing numbers" numbers))))
 
 (defn- lotto-numbers
   []
@@ -25,8 +64,9 @@
   (loop [numbers []]
     (if (= n (count numbers))
       numbers
-      (let [chosen-numbers (number-fn)]
-        (recur (conj numbers chosen-numbers))))))
+      (if-let [chosen-numbers (number-fn)]
+        (recur (conj numbers chosen-numbers))
+        (recur numbers)))))
 
 (defn -main
   [& args]
