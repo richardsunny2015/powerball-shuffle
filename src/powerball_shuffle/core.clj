a78833ee5c2fc0d5dc4b3d32f7e335272db94bf3
Richard Lanchez <richardsunny2015@gmail.com>
2025-09-05 12:55:08 -0400

    Add fn for counting frequencies for numbers

--- src/powerball_shuffle/core.clj
+++ src/powerball_shuffle/core.clj
@@ -1,5 +1,6 @@
 (ns powerball-shuffle.core
-  (:require [clojure.data.csv :as csv]
+  (:require [cheshire.core :as json]
+            [clojure.data.csv :as csv]
             [clojure.java.io :as io]
             [clojure.string :as str]))
 
@@ -10,20 +11,22 @@
             (repeat))
        (rest csv-data)))
 
+(defn- get-winning-numbers
+  [record]
+  (-> record
+      (get "Winning Numbers")
+      (str/split #" ")))
+
 (defn- massage-record
   [record]
-  (let [winning-numbers (-> record 
-                            (get "Winning Numbers")
-                            (str/split #" "))]
+  (let [winning-numbers (get-winning-numbers record)]
     (mapv #(Integer/parseInt %) winning-numbers)))
 
 (defn- massage-records
   "Takes sets of records and returns the winning
    numbers as a set."
   [records]
-  (->> records 
-       (map massage-record)
-       (set)))
+  (map massage-record records))
 
 (defn- read-previous-powerball-numbers
   "Reads csv file from PAST_POWERBALL env variable
@@ -33,7 +36,8 @@
     (let [results (-> r
                       (csv/read-csv)
                       (csv-data->maps))]
-      (massage-records results))))
+      (-> (massage-records results)
+          (doall)))))
 
 (def previous-powerball-numbers (read-previous-powerball-numbers))
 
@@ -47,8 +51,9 @@
         powerball (->> (range 1 27)
                        (shuffle)
                        (first))
-        numbers (conj main-numbers powerball)]
-    (if-not (contains? previous-powerball-numbers numbers)
+        numbers (conj main-numbers powerball)
+        previous-number-set (set previous-powerball-numbers)]
+    (if-not (contains? previous-number-set numbers)
       numbers
       (println "Generated existing numbers" numbers))))
 
@@ -68,6 +73,31 @@
         (recur (conj numbers chosen-numbers))
         (recur numbers)))))
 
+(defn- count-frequencies
+  [numbers-to-count]
+  (->> (reduce
+        (fn [acc numbers]
+          (reduce
+           (fn [acc number]
+             (update acc number (fnil inc 0)))
+           acc
+           numbers))
+        {}
+        numbers-to-count)
+       (into [])
+       (sort-by second)))
+
+(defn- count-powerball-frequencies
+  []
+  (let [main-numbers (map drop-last previous-powerball-numbers)
+        powerball (map (fn [xs] [(last xs)]) previous-powerball-numbers)
+        main-number-count (count-frequencies main-numbers)
+        powerball-count (count-frequencies powerball)]
+    (with-open [w1 (io/writer "target/main-number-counts.json")
+                w2 (io/writer "target/powerball-counts.json")]
+      (json/generate-stream main-number-count w1 {:pretty true})
+      (json/generate-stream powerball-count w2 {:pretty true}))))
+
 (defn -main
   [& args]
   (let [n (Integer/parseInt (first args)) ;; amount of sets of powerball numbers to generate
@@ -80,3 +110,8 @@
       (->> number-set
            (str/join " ")
            (println)))))
+
+(comment 
+  
+  (count-powerball-frequencies)
+  )
